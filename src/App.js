2893a097f11d8c6a96d0c78623eeb660e2636570
Shuseynzada <sh.samxal@gmail.com>
2022-11-17 15:48:17 +0400

    besdi

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <ion-icon name="arrow-forward-outline"></ion-icon>
-      <ion-icon name="logo-facebook"></ion-icon>
+      <div id='header'>
+        <div id='icons'>
+          <ion-icon id="face" name="logo-facebook"></ion-icon>
+          <ion-icon id="insta" name="logo-instagram"></ion-icon>
+          <ion-icon id="youtube" name="logo-youtube"></ion-icon>
+          <ion-icon id="mail" name="mail-outline"></ion-icon>
+        </div>
+        <div>
+          ADALI MAGAZINE
+        </div>
+        <div>
+          <ion-icon name="search-outline"></ion-icon>
+        </div>
+      </div>
+      <div>
+        <nav id= "nav-bar">
+          <div>HOME</div>
+          <div>CATEGORIES</div>
+          <div>MAGAZINE</div>
+          <div>GALLERY</div>
+          <div>CONTACT US</div>
+        </nav>
+      </div>
     </div>
   );
 }
